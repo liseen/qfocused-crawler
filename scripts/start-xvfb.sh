9af3806c3e9f4a9446f9e25ae779da814f36ffbe
liseen <liseen.wan@gmail.com>
2010-04-15 17:56:17 +0800

    fixed a config error

--- scripts/start-xvfb.sh
+++ scripts/start-xvfb.sh
@@ -9,4 +9,4 @@ cd $BIN/..
 #ulimit -v $(( 2048 * 1024 ))
 #ulimit -t 1800
 
-exec Xvfb :99 -auth scripts/Xauth.conf -shmem -dpi 96 -nolisten tcp -screen 1 1280x1024x8 2>/dev/null &
+exec Xvfb :99 -auth etc/Xauth.conf -shmem -dpi 96 -nolisten tcp -screen 1 1280x1024x8 2>/dev/null &
