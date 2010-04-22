95a0b1c68835ff6522a9df587052d06b62b299b9
liseen <liseen.wan@gmail.com>
2010-04-22 21:54:47 +0800

    fixed a bug

--- src/qcrawler_storage_record.cpp
+++ src/qcrawler_storage_record.cpp
@@ -3,10 +3,10 @@
 
 int QCrawlerStorageRecord::process(QCrawlerRecord &rec) {
     if (! crawler_db->updateUrlStatus(rec.crawl_url().url(), QCrawlerUrl::CRAWLED_OK) ) {
-        return -1;
+        return 1;
     }
     if (!crawler_db->storeRecord(rec)) {
-        return -1;
+        return 1;
     }
 
     return 0;
