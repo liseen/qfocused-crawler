251fe3b2b8564133ca8a565ec2a707bafd8993bd
liseen <liseen.wan@gmail.com>
2010-04-26 10:33:46 +0800

    added url check for get task

--- src/qcrawler_get_task.cpp
+++ src/qcrawler_get_task.cpp
@@ -37,6 +37,12 @@ int QCrawlerGetTask::process(QCrawlerRecord &rec) {
     QByteArray serial_bytes;
     if (url_queue->shift(&serial_bytes)) {
         valid = crawl_url.parse_from_bytes(serial_bytes);
+        if (valid) {
+            QUrl qurl(crawl_url.url());
+            valid = qurl.isValid();
+        } else {
+            fprintf(stderr, "invalid url: %s\n", crawl_url.url().toUtf8().constData());
+        }
     } else {
         if (QCrawlerConfig::getInstance()->quit_on_no_url_found()) {
             exit(2);
@@ -48,7 +54,7 @@ int QCrawlerGetTask::process(QCrawlerRecord &rec) {
     }
 
     // enabled freq control
-    if (QCrawlerConfig::getInstance()->need_freq_control()) {
+    if (valid && QCrawlerConfig::getInstance()->need_freq_control()) {
         while (!freq_control->canCrawl(crawl_url.host())) {
             QCrawlerSleep::sleep(1);
         }
